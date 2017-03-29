81c4a5533cd9ba584144e57c832c2efa4223d9b7
Kimberly <mccartyt1@montclair.edu>
2017-03-29 18:41:30 -0400

    added some more stubs for db

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,49 @@
 #! /usr/bin/python3
+"""
+Rough webapp for final
+This script is basic and monolithic
+"""
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort,\
     render_template, flash
+
+app = Flask(__name__) # start a Flask webapp from this instance
+app.config.from_object(__name__) #Use config variables in this file
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, "data.db"),
+    SECRET_KEY="development_key",
+    USERNAME="ADMIN",
+    PASSWORD="default"))
+
+app.config.from_envvar('CYBERSLUTH_SETTINGS', silent=True)
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_db();
+    return g.sqlite_db;
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
+
+
+def init_db():
+    db = get_db()
+    with app.open_resource('db_init/schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit();
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
