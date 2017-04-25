6dd12348124d84e66b6155051c90273a5b42edde
Kimberly <mccartyt1@montclair.edu>
2017-04-24 22:18:37 -0400

    Finished basic login, expanded schema for more customer info

--- webapp/core.py
+++ webapp/core.py
@@ -4,6 +4,12 @@ Rough webapp for final
 This script is basic and monolithic
 I will probably split it later
 """
+#FIXME: add field for user first name and last name
+#Fixme: add email field and verification link
+#Fixme: Add cell phone number
+#FIXME: add Home address and social security number
+#FIXME: Forgot password link
+#FIXME: add lockout
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort,\
@@ -13,6 +19,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort,\
 #I will use pbkdf2, a hashing algorithm considered "good" for passwords, and definitely better than just SHA-1.
 import hashlib
 import binascii
+import base64
 import hmac
 
 import time #Used for TOTP algorithm
@@ -22,16 +29,16 @@ HASH_ROUNDS = 100000 # constant for how many rounds of SHA-256 to run on passwor
 app = Flask(__name__) # start a Flask webapp from this instance
 app.config.from_object(__name__) #Use config variables in this file
 
-#the g object serves as a place to store global data between requests and sessions
-
 
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, "data.db"),
-    SECRET_KEY="development_key",
-    USERNAME="ADMIN",
+    RESET_DB = True, #purge and recreate DB on user registration, useful for testing
+    SKIP_TOTP = True, #Consider all codes valid, useful for testing
+    DUMP_TABLES = False, #Print contents of tables on insert
+    SECRET_KEY="development_key", #replace in production, used to encrypt sessions cookie to prevent user modification.
     APPNAME = "Monster Lockdown Security System",
     ORGNAME = "Cybersleuth Security",
-    PASSWORD="default"))
+    ))
 
 app.config.from_envvar('CYBERSLEUTH_SETTINGS', silent=True)
 
@@ -40,8 +47,7 @@ app.config.from_envvar('CYBERSLEUTH_SETTINGS', silent=True)
 def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
-    #FIXME line below will erase all data on startup, should be removed
-    #But great for testing!
+    #NOTE commented line below will erase all data on startup, should be removed unless testing
     #init_db(rv) #uncomment to init db
     return rv
 
@@ -74,9 +80,37 @@ def get_user(username):
     result = cur.fetchone()
     return result
 
+#find url for an image based on its title
+def get_image_url(image_title):
+    db = get_db()
+    cur = db.execute("select * FROM images WHERE title=?", (image_title,))
+    result = cur.fetchone()
+    return url_for("static", filename=result['file'])
+
+def get_random_images():
+    db = get_db()
+    cur = db.execute("select * FROM images ORDER BY RANDOM() LIMIT 8")
+    result = []
+    for row in cur.fetchall():
+        result.append({'title': row['title'], 'file': url_for("static", filename=row['file'])})
+    return result
+
+def add_attempt(username):
+    db = get_db()
+    cur = db.execute("update accounts set attempts = attempts + 1 where user=?", (username,))
+    db.commit()
+
+def reset_attempts(username):
+    db = get_db()
+    cur = db.execute("update accounts set attempts = 0 where user=?", (username,))
+    db.commit()
+
+def get_attempts(username):
+    db = get_db()
+    cur = db.execute("select attempts from accounts where user=?", (username,))
+    return int(cur.fetchone()[0])
 
 """VIEWS"""
-#FIXME
 @app.route('/')
 def homepage():
     return render_template('home.html')
@@ -85,45 +119,96 @@ def homepage():
 #display login prompt
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = dict(foo='none')
     if request.method == 'POST':
-        this_user = get_user(request.form["username"])
+        session['user'] = request.form['username']
+        this_user = get_user(session['user'])
         if this_user:
             #check_password
             valid_user = verify_password(this_user['user'], request.form['password'])
             if valid_user:
-                #TODO verify image
-                pass
+                session['valid_password'] = True
+                print("should go to image_select?")
+                return redirect(url_for('image_select'))
         else:
             valid_user = False
-
-        if valid_user:
-            session['logged_in'] = True;
-            session['username'] = request.form['username']
-            flash("You were logged in")
-            return redirect(url_for("homepage"))
-        else:
-            session.pop('logged_in', None)
-            #FIXME include other auth failures
+            revoke(session)
             flash("Invalid Username or Password")
             return redirect(url_for("login"))
-    #if this is not a post, return the login page
-    return render_template('login.html', error=error)
 
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash("You were logged out")
-    return redirect(url_for('homepage'))
+    #if this is a get request, return the login page
+    return render_template('login.html')
+
+@app.route('/image_select', methods=['GET', 'POST'])
+def image_select():
+    if request.method == 'POST':
+        if session.get('user'):
+            user = get_user(session['user'])
+            if request.form['sec-image'] == user['image']:
+                session['valid_image']=True
+                return redirect(url_for('totp_entry'))
+        else:
+            flash("Invalid image choice, please try again")
+            revoke(session)
+            return redirect(url_for('login'))
+    else:
+        #FIXME: add user image and 8 random ones
+        if session.get('user'):
+            user = get_user(session['user'])
+            sec_images = [{'title': user['image'], 'file': get_image_url(user['image'])}]
+            sec_images.extend(get_random_images())
+            return render_template("pictures.html", sec_images=sec_images)
+        else:
+            revoke(session)
+            return redirect(url_for('login'))
+
+
+@app.route('/totp_entry', methods=['GET', 'POST'])
+def totp_entry():
+    if request.method == "POST":
+        if session.get('user'):
+            user = get_user(session['user'])
+            totpIsValid = verify_TOTP(user['totp_token'], request.form['code'])
+            if totpIsValid:
+                session['valid_totp'] = True
+                return redirect(url_for("security_question_entry"))
+        #Fall through if invalid code
+        revoke(session)
+        flash("Invalid code.  Please try again")
+        return redirect(url_for('login'))
+    else:
+        return render_template('totp_entry.html')
+
+@app.route('/sec_question_entry', methods=['GET', 'POST'])
+def security_question_entry():
+    if session.get('user') and session['valid_totp']:
+        user = get_user(session['user'])
+        if request.method == 'POST':
+            #FIXME add hashing for security answer
+            if user['security_answer'] == request.form['security_answer']:
+                session['logged_in'] = True
+                return redirect(url_for('homepage'))
+            else:
+                revoke(session)
+                flash("Incorrect answer")
+                return redirect(url_for('login'))
+        else:
+            security_question = user['security_question']
+            return render_template("security_question_entry.html", security_question = security_question)
+    else:
+        revoke(session)
+        return redirect(url_for('login'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if session.get('logged_in'):
         flash("Please log out to register a new account.")
         return redirect(url_for('homepage'))
-    elif request.method == 'POST':
+    else:
+        revoke(session) #Clear cookie/session for safety
+    if request.method == 'POST':
         f = request.form
-        reg_status = register_account(f['username'], f['passwd'], f['sec-image'], f['totp'], f['question'], f['answer'], 0)
+        reg_status = register_account(f['username'], f['passwd'], f['sec-image'], f['totp'], f['question'], f['answer'], f['email'], f['home_address'], f['phone_number'], f['social_security'])
         if reg_status['success']:
             flash("Account created successfully.  Please log in.")
             return redirect(url_for("login"))
@@ -133,12 +218,17 @@ def register():
     #default return and GET response
     return render_template('register.html')
 
+
+@app.route('/logout')
+def logout():
+    revoke(session)
+    flash("You are now logged out")
+    redirect(url_for("homepage"))
+
+
 #TODO 404 page
-#TODO TOTP (time based onetime pass)
 
 """CONTROLLER"""
-#TODO functions to authenticate user
-#FIXME stub
 def verify_password(user, plain_passwd):
     db = get_db()
     cur = db.execute("select passwd, salt FROM accounts WHERE user=?", (user,))
@@ -160,7 +250,10 @@ def verify_password(user, plain_passwd):
 
 #TOTP algorithm that takes a private key, a code from the user, a starting time after the unix epoch, and a time interval.  Here, we default to 0 (0 seconds after Jan 1st, 1970 in time library) and 30 seconds (a reasonable time for the user to enter a code)
 def verify_TOTP(key, code, epoch=0, timeInterval=30):
-    unix_time = time.gmtime()
+    if app.config['SKIP_TOTP']:
+        return True
+
+    unix_time = time.time()
     steps = (unix_time - epoch) // timeInterval;
     valid_codes = [generate_HOTP(key, steps + x) for x in range(-1, 2)] # see what codes are okay for one step ago, now, and one step in the future
     # These three codes account for clock skew and slow user input.
@@ -175,9 +268,11 @@ def verify_TOTP(key, code, epoch=0, timeInterval=30):
 #https://en.wikipedia.org/wiki/Google_Authenticator
 #http://stackoverflow.com/a/23221582
 def generate_HOTP(key, steps):
-    hash = hmac.new(key, steps, hashlib.sha1).digest()
-    #following two lines are from stackoverflow answer
-    offset = int(hash[-1, 16])
+    try:
+        hash = hmac.new(base64.b32decode(key), steps, hashlib.sha1)            #following two lines are from stackoverflow answer
+    except binascii.Error:
+        return 0
+        offset = int(hash[-1, 16])
     binary = int(hash[(offset * 2):((offset * 2) + 8)], 16) & 0x7fffffff
     return str(binary)[-6:] #take last six digits
 
@@ -186,27 +281,25 @@ def verify_image(image, correct_image):
 
 #TODO FIXME
 #NOTE consider hash+salt for answer
-def register_account(username, passwd, image, totp, security_question, security_answer, is_admin=False):
+def register_account(username, passwd, image, totp, security_question, security_answer, email, home_address, phone_number, social_security, is_admin=False, attempts=0):
     status = {'success': False, 'message': "Failure: Cause unknown"}
     salt = create_salt()
-    #FIXME validate username
-    #FIXME validate passwd
-    #FIXME validate image
     totp = 0 #FIXME placeholder value until totp is implemented.
     hashed_passwd = hash_password(passwd, binascii.unhexlify(salt))
 
     db = get_db()
+    if app.config['RESET_DB']: init_db(db)
     db.execute("insert into accounts " + \
-               "values (?, ?, ?, ?, ?, ?, ?, ?)", \
-               (username, is_admin, hashed_passwd, salt, totp, image, security_question, security_answer)
+               "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", \
+               (username, is_admin, email, home_address, phone_number, social_security, hashed_passwd, salt, totp, image, security_question, security_answer, attempts)
     )
     db.commit()
     status['success'] = True
     status['message'] = "User registered"
-    #TODO remove this in final version
-    for row in db.execute("select * from accounts"):
-        for key_ in row.keys():
-            print ("{}: {}".format(key_, row[key_]))
+    if app.config['DUMP_TABLES']:
+        for row in db.execute("select * from accounts"):
+            for key_ in row.keys():
+                print ("{}: {}".format(key_, row[key_]))
     return status
 
 def hash_password(passwd, salt):
@@ -223,6 +316,18 @@ def create_salt():
     binary_salt = os.urandom(32)
     return binascii.hexlify(binary_salt)
 
-#Python idiom that more or less means, if we're running this script manually, run this code.
+
+#Clear out validation information between login attempts
+def revoke(session):
+    session.pop('user', None)
+    session.pop('valid_password', None)
+    session.pop('valid_image', None)
+    session.pop('valid_totp', None)
+    session.pop('valid_sec_question', None)
+    session.pop('logged_in', None)
+
+#Python idiom that more or less means:
+#if we're running this script from the command line
+#instead of from a server, run this code.
 if __name__ == "__main__":
     app.run()
--- webapp/db_init/schema.sql
+++ webapp/db_init/schema.sql
@@ -1,16 +1,32 @@
 drop table if exists accounts;
 create table accounts (
-       'user' text not null,
+       'user' text not null unique,
        isAdministrator integer not null,
+       email not null,
+       home_address not null,
+       phone_number not null,
+       social_security not null,
        passwd text not null,
        salt text not null,
        totp_token text not null,
        image text not null,
        security_question not null,
-       security_answer not null
+       security_answer not null,
+       attempts integer not null default 0
 );
--- TODO To consider: Is an administrator a type of user, or a different entity, in this model?
 drop table if exists images;
 create table images (
-       title text not null
+       title text not null,
+       file text not null
 );
+-- populate default images
+insert into images values ("apple", "apple.png");
+insert into images values ("bird", "bird.png");
+insert into images values ("building", "building.png");
+insert into images values ("car", "car.png");
+insert into images values ("guitar", "guitar.png");
+insert into images values ("money", "money.png");
+insert into images values ("mug", "mug.png");
+insert into images values ("robot", "robot.png");
+insert into images values ("stop sign", "stop-sign.png");
+insert into images values ("tree", "tree.png");
